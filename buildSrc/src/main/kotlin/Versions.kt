00c0eef32da168805a6c8831d6742c3c1694015a
Segun Francis <francis4dblues@gmail.com>
2021-09-09 09:03:52 +0100

    Initial project setup
    * Creation of various modules
    * Dependency management with BuildSrc

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,27 @@
+object Versions {
+    const val compileSdkVersion: Int = 31
+    const val minSdkVersion: Int = 21
+    const val buildToolsVersion = "30.0.3"
+
+    const val jvmTarget: String = "1.8"
+    const val coreKtxVersion: String = "1.6.0"
+    const val appCompatVersion: String = "1.3.1"
+    const val materialVersion = "1.4.0"
+    const val constraintLayoutVersion = "2.1.0"
+    const val hiltVersion = "2.38.1"
+    const val retrofitVersion = "2.9.0"
+    const val loggingInterceptorVersion = "4.9.1"
+    const val glideVersion = "4.12.0"
+    const val roomVersion = "2.3.0"
+    const val lottieVersion = "4.0.0"
+    const val navigationVersion = "2.3.0"
+    const val gradlePluginVersion = "1.5.30"
+    const val gradleVersion = "4.1.0"
+    const val kotlinVersion = "1.5.10"
+    const val gsonVersion = "2.8.8"
+    const val coroutineVersion = "1.5.0"
+
+    const val jUnitVersion = "4.13.2"
+    const val jUnitExtVersion = "1.1.3"
+    const val espressoVersion = "3.4.0"
+}
