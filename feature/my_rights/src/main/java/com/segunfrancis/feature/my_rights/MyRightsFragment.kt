ca1a51cc7efc84d8c63eefa69a9e5dfa95d15d71
Segun Francis <francis4dblues@gmail.com>
2021-11-22 16:17:57 +0100

    update bottom sheet height

--- feature/my_rights/src/main/java/com/segunfrancis/feature/my_rights/MyRightsFragment.kt
+++ feature/my_rights/src/main/java/com/segunfrancis/feature/my_rights/MyRightsFragment.kt
@@ -1,9 +1,12 @@
 package com.segunfrancis.feature.my_rights
 
+import android.app.Dialog
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -47,6 +50,19 @@ class MyRightsFragment : BottomSheetDialogFragment() {
                     rightsPagingAdapter.submitData(pagingData)
                 }
         }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.setOnShowListener {
+            val bottomSheet = dialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+            val params = bottomSheet.layoutParams
+            val height = Resources.getSystem().displayMetrics.heightPixels
+            val maxHeight = (height * 1.0).toInt()
+            params.height = maxHeight
+            bottomSheet.layoutParams = params
+        }
+        return dialog
     }
 
     override fun getTheme(): Int {
